a69c0e75fa1c60554c6f4a09c76d4310dd4f353f
Sid Sachdev <sid.sachdev9@yahoo.ca>
2018-07-06 00:04:55 -0400

    - Added Tests

--- FlaskSecurityDevice.py
+++ FlaskSecurityDevice.py
@@ -96,7 +96,32 @@ if __name__ == '__main__':
 """
 Test Runs Provided:
 
+1. Return All device data from uuid 1 from 1501868891 until 1591409691
+
+device_uuid = 1
+start_time = 1501868891
+end_time = 1591409691
+
+http://ec2-54-237-164-186.compute-1.amazonaws.com:8000/getdata/1/1501868891/1591409691
 
 
+2. Return All device data from uuid 43 from 1501868891 until 1591409691
+
+device_uuid = 43
+start_time = 1501868891
+end_time = 1591409691
+
+http://ec2-54-237-164-186.compute-1.amazonaws.com:8000/getdata/43/1501868891/1591409691
+
+
+3. Insert New Data for uuid 
+127.0.0.1:5000/postdata?device_uuid=4&sensor_type=humidity&sensor_value=100&sensor_reading_time=1593292
+
+device_uuid = 4
+sensor_type = humidity
+sensor_value = 100
+sensor_reading_time = 1593292
+
+http://ec2-54-237-164-186.compute-1.amazonaws.com:8000/postdata?device_uuid=4&sensor_type=humidity&sensor_value=100&sensor_reading_time=1593292
 
 """
